689f17c831d8683ce6a72d26297ed10bc4bd1bd9
Ruslan Kapisaski <ruslank1@abv.bg>
2025-02-24 19:21:52 +0200

    Updated

--- #1 Asynchronous Programming/01.Bus-Stop/app.js
+++ #1 Asynchronous Programming/01.Bus-Stop/app.js
@@ -1,79 +1,79 @@
-// //AJAX
-// function getInfoAJAX() {
-//   let resultDiv = document.getElementById("result");
-//   let stopNameDiv = resultDiv.querySelector("#stopName");
-//   let busUl = resultDiv.querySelector("#buses");
-//   const stopIdRef = document.getElementById("stopId");
-//   const validIds = [1287, 1308, 1327, 2334];
+//AJAX
+function getInfoAJAX() {
+  let resultDiv = document.getElementById("result");
+  let stopNameDiv = resultDiv.querySelector("#stopName");
+  let busUl = resultDiv.querySelector("#buses");
+  const stopIdRef = document.getElementById("stopId");
+  const validIds = [1287, 1308, 1327, 2334];
 
-//   if (!validIds.includes(Number(stopIdRef.value))) {
-//     stopNameDiv.textContent = "Error";
-//     clear();
-//     return;
-//   }
+  if (!validIds.includes(Number(stopIdRef.value))) {
+    stopNameDiv.textContent = "Error";
+    clear();
+    return;
+  }
 
-//   const url = `http://localhost:3030/jsonstore/bus/businfo/${stopIdRef.value}`;
-//   const httpRequest = new XMLHttpRequest();
+  const url = `http://localhost:3030/jsonstore/bus/businfo/${stopIdRef.value}`;
+  const httpRequest = new XMLHttpRequest();
 
-//   httpRequest.addEventListener("readystatechange", () => {
-//     if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-//       let response = JSON.parse(httpRequest.responseText);
-//       stopIdRef.value = "";
-//       stopNameDiv.textContent = response.name;
-//       for (const bus in response.buses) {
-//         let minutes = response.buses[bus];
-//         let li = document.createElement("li");
-//         li.textContent = `Bus ${bus} arrives in ${minutes} minutes`;
-//         busUl.appendChild(li);
-//       }
-//     }
-//   });
+  httpRequest.addEventListener("readystatechange", () => {
+    if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+      let response = JSON.parse(httpRequest.responseText);
+      stopIdRef.value = "";
+      stopNameDiv.textContent = response.name;
+      for (const bus in response.buses) {
+        let minutes = response.buses[bus];
+        let li = document.createElement("li");
+        li.textContent = `Bus ${bus} arrives in ${minutes} minutes`;
+        busUl.appendChild(li);
+      }
+    }
+  });
 
-//   httpRequest.open("GET", url);
-//   httpRequest.send();
+  httpRequest.open("GET", url);
+  httpRequest.send();
 
-//   function clear() {
-//     stopIdRef.value = "";
-//     busUl.innerHTML = "";
-//   }
-// }
+  function clear() {
+    stopIdRef.value = "";
+    busUl.innerHTML = "";
+  }
+}
 
-// //Async Await
-// async function getInfoAsync() {
-//   let resultDiv = document.getElementById("result");
-//   let stopNameDiv = resultDiv.querySelector("#stopName");
-//   let busUl = resultDiv.querySelector("#buses");
-//   const stopIdRef = document.getElementById("stopId");
+//Async Await
+async function getInfoAsync() {
+  let resultDiv = document.getElementById("result");
+  let stopNameDiv = resultDiv.querySelector("#stopName");
+  let busUl = resultDiv.querySelector("#buses");
+  const stopIdRef = document.getElementById("stopId");
 
-//   const url = `http://localhost:3030/jsonstore/bus/businfo/${stopIdRef.value}`;
+  const url = `http://localhost:3030/jsonstore/bus/businfo/${stopIdRef.value}`;
 
-//   try {
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     stopIdRef.value = "";
-//     renderList(data);
-//   } catch (error) {
-//     stopNameDiv.textContent = "Error";
-//     clear();
-//   }
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    stopIdRef.value = "";
+    renderList(data);
+  } catch (error) {
+    stopNameDiv.textContent = "Error";
+    clear();
+  }
 
-//   function renderList(data) {
-//     Object.entries(data.buses).forEach((element) => {
-//       const [busId, timeToArrive] = element;
-//       const li = document.createElement("li");
-//       li.textContent = `Bus ${busId} arrives in ${timeToArrive} minutes`;
-//       busUl.appendChild(li);
-//     });
-//   }
+  function renderList(data) {
+    Object.entries(data.buses).forEach((element) => {
+      const [busId, timeToArrive] = element;
+      const li = document.createElement("li");
+      li.textContent = `Bus ${busId} arrives in ${timeToArrive} minutes`;
+      busUl.appendChild(li);
+    });
+  }
 
-//   function clear() {
-//     stopIdRef.value = "";
-//     busUl.innerHTML = "";
-//   }
-// }
+  function clear() {
+    stopIdRef.value = "";
+    busUl.innerHTML = "";
+  }
+}
 
 //Promises
-function getInfo() {
+function getInfoPromises() {
   let resultDiv = document.getElementById("result");
   let stopNameDiv = resultDiv.querySelector("#stopName");
   let busUl = resultDiv.querySelector("#buses");
