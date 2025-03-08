f9e41db5a7eb1ab2684ddc5590251ec4aca9e4e9
Ruslan Kapisaski <ruslank1@abv.bg>
2025-03-08 16:34:04 +0200

    Simple messanger application

---
+++ #2 Data And Authentification/01.Messenger/app.js
@@ -0,0 +1,52 @@
+function attachEvents() {
+  const submitEl = document.getElementById("submit");
+  const refreshEl = document.getElementById("refresh");
+  const authorRef = document.querySelector(`[name="author"]`);
+  const contentRef = document.querySelector(`[name="content"]`);
+  const textArea = document.getElementById("messages");
+
+  const baseUrl = `http://localhost:3030/jsonstore/messenger`;
+
+  submitEl.addEventListener("click", postData);
+  refreshEl.addEventListener("click", getData);
+
+  async function postData(e) {
+    const data = {
+      author: authorRef.value,
+      content: contentRef.value,
+    };
+
+    let responce = await fetch(baseUrl, {
+      method: "post",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    if (!responce.ok) {
+      return alert("Error posting a content");
+    }
+
+    authorRef.value = "";
+    contentRef.value = "";
+
+    let result = await responce.json();
+  }
+
+  //"{author}: {message}"
+  async function getData(e) {
+    let responce = await fetch(`http://localhost:3030/jsonstore/messenger`);
+    let result = await responce.json();
+    let values = Object.values(result);
+    console.log(values);
+    textArea.disabled = false;
+    for (const el of values) {
+      let p = document.createElement("p");
+      textArea.value += `${el.author}: ${el.content}\n`;
+      console.log(textArea.value);
+      textArea.append(p);
+    }
+  }
+}
+
+attachEvents();
